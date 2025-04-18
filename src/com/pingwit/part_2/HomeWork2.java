93c4d173709df1e993a063d7ade9a967f84f72fc
YoungFimozzz <dragonklaim@gmail.com>
2025-04-18 17:50:10 +0200

    Update HomeWork2.java

--- src/com/pingwit/part_2/HomeWork2.java
+++ src/com/pingwit/part_2/HomeWork2.java
@@ -2,6 +2,6 @@ package com.pingwit.part_2;
 
 public class HomeWork2 {
     public static void main(String[] args) {
-        System.out.println(208 / 4);
+        System.out.println(168 / 4);
     }
 }
